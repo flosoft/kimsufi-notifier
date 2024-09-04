edb654d6bdda58ce10c22ba50065be59f0af7a9a
Theo Brigitte <theo.brigitte@gmail.com>
2024-09-04 13:59:39 +0200

    Move telegram bot into pkg/telegram package

--- src/cmd/bot/bot.go
+++ src/cmd/bot/bot.go
@@ -2,15 +2,11 @@ package bot
 
 import (
 	"fmt"
-	"os"
-	"strings"
-	"time"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	tele "gopkg.in/telebot.v3"
 
-	"github.com/TheoBrigitte/kimsufi-notifier/pkg/kimsufi"
+	"github.com/TheoBrigitte/kimsufi-notifier/pkg/telegram"
 )
 
 var (
@@ -33,84 +29,17 @@ func init() {
 func runner(cmd *cobra.Command, args []string) error {
 	level, err := log.ParseLevel(logLevel)
 	if err != nil {
-		log.Fatalf("failed to parse log-level: %v\n", err)
+		return fmt.Errorf("failed to parse log-level: %w", err)
 	}
 	log.SetLevel(level)
 
-	pref := tele.Settings{
-		Token:  os.Getenv("TOKEN"),
-		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
-	}
-
-	b, err := tele.NewBot(pref)
+	telegramBot, err := telegram.NewBot()
 	if err != nil {
-		log.Fatal(err)
-		return err
+		return fmt.Errorf("failed to initialize telegram bot: %w", err)
 	}
-
-	b.Handle("/hello", func(c tele.Context) error {
-		username := c.Sender().Username
-
-		return c.Send("Hello! @" + username)
-	})
-
-	b.Handle("/help", func(c tele.Context) error {
-		return c.Send("Available commands: /help, /subscribe, /unsubscribe, /countries, /datacenters, /plans")
-	})
-
-	b.Handle("/subscribe", func(c tele.Context) error {
-		username := c.Sender().Username
-		fmt.Printf("payload: %s\n", c.Message().Payload)
-		var planCode string
-		var datacentersString string
-		n, err := fmt.Sscan(c.Message().Payload, &planCode, &datacentersString)
-		if err != nil {
-			fmt.Println("Invalid command")
-			return err
-		}
-		var datacenters = strings.Split(strings.Trim(datacentersString, ","), ",")
-		fmt.Printf("scanned %d variables: %s, %v\n", n, planCode, datacenters)
-
-		var datacentersMessage string
-		if len(datacenters) > 0 {
-			datacentersMessage = "one of the following datacenters"
-		} else {
-			datacentersMessage = "this datacenter"
-		}
-		subscriptionId := 1
-
-		return c.Send(fmt.Sprintf("@%s you will be notified when plan %s is available in %s %s (subscriptionId: %d)", username, planCode, datacentersMessage, strings.Join(datacenters, ", "), subscriptionId))
-	})
-
-	b.Handle("/unsubscribe", func(c tele.Context) error {
-		username := c.Sender().Username
-		fmt.Printf("payload: %s\n", c.Message().Payload)
-		var subscriptionId int
-		n, err := fmt.Sscan(c.Message().Payload, &subscriptionId)
-		if err != nil {
-			fmt.Println("Invalid command")
-			return err
-		}
-
-		fmt.Printf("scanned %d variables: %d\n", n, subscriptionId)
-
-		return c.Send(fmt.Sprintf("@%s you unsubscribed from subscription %d", username, subscriptionId))
-	})
-
-	b.Handle("/countries", func(c tele.Context) error {
-		return c.Send(fmt.Sprintf("Allowed countries: %s", strings.Join(kimsufi.AllowedCountries, ", ")))
-	})
-
-	b.Handle("/datacenters", func(c tele.Context) error {
-		return c.Send(fmt.Sprintf("Allowed datacenters: %s", strings.Join(kimsufi.AllowedDatacenters, ", ")))
-	})
-
-	b.Handle("/plans", func(c tele.Context) error {
-		return c.Send("Allowed plans: 1cpu-1gb, 1cpu-2gb, 2cpu-4gb, 2cpu-8gb, 4cpu-8gb, 4cpu-16gb, 8cpu-16gb, 8cpu-32gb, 16cpu-32gb, 16cpu-64gb, 32cpu-64gb, 32cpu-128gb")
-	})
 
 	fmt.Println("Bot is running")
-	b.Start()
+	telegramBot.Start()
 
 	return nil
 }
---
+++ src/pkg/telegram/bot.go
@@ -0,0 +1,87 @@
+package telegram
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	tele "gopkg.in/telebot.v3"
+
+	"github.com/TheoBrigitte/kimsufi-notifier/pkg/kimsufi"
+)
+
+func NewBot() (*tele.Bot, error) {
+	pref := tele.Settings{
+		Token:  os.Getenv("TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		return nil, err
+	}
+
+	b.Handle("/hello", func(c tele.Context) error {
+		username := c.Sender().Username
+
+		return c.Send("Hello! @" + username)
+	})
+
+	b.Handle("/help", func(c tele.Context) error {
+		return c.Send("Available commands: /help, /subscribe, /unsubscribe, /countries, /datacenters, /plans")
+	})
+
+	b.Handle("/subscribe", func(c tele.Context) error {
+		username := c.Sender().Username
+		fmt.Printf("payload: %s\n", c.Message().Payload)
+		var planCode string
+		var datacentersString string
+		n, err := fmt.Sscan(c.Message().Payload, &planCode, &datacentersString)
+		if err != nil {
+			fmt.Println("Invalid command")
+			return err
+		}
+		var datacenters = strings.Split(strings.Trim(datacentersString, ","), ",")
+		fmt.Printf("scanned %d variables: %s, %v\n", n, planCode, datacenters)
+
+		var datacentersMessage string
+		if len(datacenters) > 0 {
+			datacentersMessage = "one of the following datacenters"
+		} else {
+			datacentersMessage = "this datacenter"
+		}
+		subscriptionId := 1
+
+		return c.Send(fmt.Sprintf("@%s you will be notified when plan %s is available in %s %s (subscriptionId: %d)", username, planCode, datacentersMessage, strings.Join(datacenters, ", "), subscriptionId))
+	})
+
+	b.Handle("/unsubscribe", func(c tele.Context) error {
+		username := c.Sender().Username
+		fmt.Printf("payload: %s\n", c.Message().Payload)
+		var subscriptionId int
+		n, err := fmt.Sscan(c.Message().Payload, &subscriptionId)
+		if err != nil {
+			fmt.Println("Invalid command")
+			return err
+		}
+
+		fmt.Printf("scanned %d variables: %d\n", n, subscriptionId)
+
+		return c.Send(fmt.Sprintf("@%s you unsubscribed from subscription %d", username, subscriptionId))
+	})
+
+	b.Handle("/countries", func(c tele.Context) error {
+		return c.Send(fmt.Sprintf("Allowed countries: %s", strings.Join(kimsufi.AllowedCountries, ", ")))
+	})
+
+	b.Handle("/datacenters", func(c tele.Context) error {
+		return c.Send(fmt.Sprintf("Allowed datacenters: %s", strings.Join(kimsufi.AllowedDatacenters, ", ")))
+	})
+
+	b.Handle("/plans", func(c tele.Context) error {
+		return c.Send("Allowed plans: 1cpu-1gb, 1cpu-2gb, 2cpu-4gb, 2cpu-8gb, 4cpu-8gb, 4cpu-16gb, 8cpu-16gb, 8cpu-32gb, 16cpu-32gb, 16cpu-64gb, 32cpu-64gb, 32cpu-128gb")
+	})
+
+	return b, nil
+}
